b238eb28c54a3daaa8c01320397d43eafa53e849
Sanskar Verma <145315641+Sanskar325@users.noreply.github.com>
2025-02-05 23:56:05 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -52,7 +52,7 @@ const Hero = () => {
                *  change md:text-6xl, add more responsive code
                */}
               <TextGenerateEffect
-                words="<p>A Electronics and Communication Engineer</p>"
+                words="<p>An Electronics and Communication Engineer</p>"
                 className="text-center text-[40px] md:text-5xl lg:text-6xl"
               />
     
